e05d2c5e3f8c916a4897ec21852c0f08578ac4cb
mustapha diouani <mostapha37170@gmail.com>
2025-12-07 02:57:35 +0100

    ajouter le ficher javascript

--- script.js
+++ script.js
@@ -12,3 +12,27 @@ function load() {
   const raw = window.localStorage.getItem(KEY);
   state = raw ? JSON.parse(raw) : { transactions: [] };
 }
+
+function calculeTotals() {
+  const revenus = state.transactions
+    // @ts-ignore
+    .filter((t) => t.type === "revenus")
+    // @ts-ignore
+    .reduce((acc, value) => acc + value.montant, 0);
+
+  const depences = state.transactions
+    // @ts-ignore
+    .filter((t) => t.type === "depences")
+    // @ts-ignore
+    .reduce((acc, value) => acc + value.montant, 0);
+  const solde = revenus - depences;
+  return { revenus, depences, solde };
+}
+
+const btnajouter = document.getElementById("btn-ajouter");
+// @ts-ignore
+btnajouter?.addEventListener("click", () => {
+  const rev = document.getElementById("montant");
+  // @ts-ignore
+  console.log(rev.value);
+});
