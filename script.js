f8e20f588f5e1bdd88c65dd3a3eeccf1fe6421cd
mustapha diouani <mostapha37170@gmail.com>
2025-12-09 02:48:21 +0100

    la premiere version de Calculateur Budget

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
+// @ts-nocheck
 // Storage Key
 const KEY = "miniBudget:v1";
 
+// elements
+const btnajouter = document.getElementById("btn-ajouter");
+const montant = document.getElementById("montant");
+const types = document.getElementById("choix-revenus");
+const description = document.getElementById("description");
+const revenus = document.getElementById("revenus");
+const depences = document.getElementById("depences");
+const soldes = document.getElementById("solde");
 // state
 let state = { transactions: [] };
 
@@ -13,26 +22,57 @@ function load() {
   state = raw ? JSON.parse(raw) : { transactions: [] };
 }
 
-function calculeTotals() {
-  const revenus = state.transactions
-    // @ts-ignore
-    .filter((t) => t.type === "revenus")
-    // @ts-ignore
-    .reduce((acc, value) => acc + value.montant, 0);
-
-  const depences = state.transactions
-    // @ts-ignore
-    .filter((t) => t.type === "depences")
-    // @ts-ignore
-    .reduce((acc, value) => acc + value.montant, 0);
-  const solde = revenus - depences;
-  return { revenus, depences, solde };
+/**
+ * @param {{ toLocaleString: (arg0: string, arg1: { style: string; currency: string; maximumFractionDigits: number; }) => any; }} n
+ */
+function formatMoney(n) {
+  return n.toLocaleString("fr-FR", {
+    style: "currency",
+    currency: "EUR",
+    maximumFractionDigits: 0,
+  });
 }
 
-const btnajouter = document.getElementById("btn-ajouter");
+function calculeTotals() {
+  const revenu = state.transactions
+    .filter((t) => t.type === "revenus")
+    .reduce((s, t) => s + t.montants, 0);
+
+  const depence = state.transactions
+    .filter((t) => t.type === "depences")
+    .reduce((s, t) => s + t.montants, 0);
+
+  const solde = revenu - depence;
+  return { revenu, depence, solde };
+}
+
+function render() {
+  const { revenu, depence, solde } = calculeTotals();
+
+  revenus.textContent = formatMoney(revenu);
+  depences.textContent = formatMoney(depence);
+  soldes.textContent = formatMoney(solde);
+
+  save();
+}
+
 // @ts-ignore
 btnajouter?.addEventListener("click", () => {
-  const rev = document.getElementById("montant");
-  // @ts-ignore
-  console.log(rev.value);
+  event?.preventDefault();
+
+  const type = types.value;
+  const desc = description.value.trim();
+  const montants = Math.abs(parseFloat(montant.value) || 0);
+  if (montants <= 0) return alert("Indique un montant supérieur à 0");
+  const tab = { type, desc, montants, Date: new Date().toISOString() };
+
+  state.transactions.push(tab);
+  console.log(state);
+  description.value = "";
+  montant.value = "";
+
+  render();
 });
+
+load();
+render();
